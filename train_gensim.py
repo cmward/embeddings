f3d880a7a2cc3b56507884a7698214bd08408174
Chris Ward <cmward@brandeis.edu>
2015-12-11 12:19:55 -0500

    fix neg value bug

--- train_gensim.py
+++ train_gensim.py
@@ -8,6 +8,6 @@ import climate
 climate.enable_default_logging()
 
 sentences = SubdirSentences('../data/text', n_files=5500)
-model = Word2Vec(sentences, size=100, window=10, min_count=20, sample=1,
-                 workers=4, negative=1e-5, hs=0, sg=1)
+model = Word2Vec(sentences, size=100, window=10, min_count=20, sample=1e-5,
+                 workers=4, negative=5, hs=0, sg=1)
 model.save('../models/gs100')
