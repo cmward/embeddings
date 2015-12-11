b73e9e994506a75be1ce0dea7f9df9481ce7928f
Chris Ward <cmward@brandeis.edu>
2015-12-11 01:40:25 -0500

    add logging

--- train_gensim.py
+++ train_gensim.py
@@ -1,8 +1,11 @@
 from gensim.models import Word2Vec
 from corpus import SubdirSentences
+import climate
 
 """ Train a model using gensim.
 """
+
+climate.enable_default_logging()
 
 sentences = SubdirSentences('../data/text', n_files=5500)
 model = Word2Vec(sentences, size=100, window=10, min_count=20, sample=1,
